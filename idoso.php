91de4e4a9590b8084732c11bb3e865056be02660
shaq-senior-expert <senior.expert.iot@gmail.com>
2016-04-20 19:37:44 -0300

    api idosos

---
+++ idoso.php
@@ -0,0 +1,32 @@
+<?php
+require "bd.php";
+header('Content-Type: application/jason');
+
+$request = $_SERVER['REQUEST_METHOD'];
+switch ($request) {
+	case 'GET':
+		$result = mysql_query("SELECT * FROM idoso");
+		$linhas = array();
+		while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+    		$linhas[] = $row;  
+		}
+		echo json_encode($linhas);
+		break;
+	case 'POST':
+		$insert = json_decode($HTTP_RAW_POST_DATA);
+		mysql_query("INSERT INTO idoso (nome,idade,endereco,email) VALUES ('{$insert->nome}','{$insert->idade}','{$insert->endereco}','{$insert->email})");
+		break;
+	case 'PUT':
+		$update = json_decode(file_get_contents('php://input'));
+		$id = $update->id;
+		mysql_query("UPDATE idoso SET email = '{$update->email}' WHERE id={$id}");
+		break;
+	case 'DELETE':
+		$delete = json_decode(file_get_contents('php://input'));
+		$id = $delete->id;
+		mysql_query("DELETE FROM idoso WHERE id={$id}");
+		break;
+	default:
+		# code...
+		break;
+}
