171866fffd5d0fa9f862678e964931b5c0ddbd27
shaq-senior-expert <senior.expert.iot@gmail.com>
2016-04-19 22:21:55 -0300

    Alteracao do script de insercao de dados

--- insercaodedados.sql
+++ insercaodedados.sql
@@ -1,3 +1,5 @@
+--inserir dados na tabela de CONTATO
+
 insert into contato(nome, email, assunto, mensagem) values('idoso1', 'idoso1@bol.com.br', 'reclamacao', 'Estou insatifeito com os servicos prestados');
 
 insert into contato(nome, email, assunto, mensagem) values('idoso2', 'idoso2@bol.com.br', 'puxacao de saco', 'Estou extremamente satifeito com os servicos prestados');
@@ -5,4 +7,15 @@ insert into contato(nome, email, assunto, mensagem) values('idoso2', 'idoso2@bol
 insert into contato(nome, email, assunto, mensagem) values('idoso3', 'idoso3@bol.com.br', 'puxacao de saco', 'Estou extremamente satifeito com os servicos prestados');
 
 insert into contato(nome, email, assunto, mensagem) values('idoso4', 'idoso4@bol.com.br', 'puxacao de saco', 'Estou extremamente satifeito com os servicos prestados');
+
+
+
+--inserir dados na tabela de CONTEUDO
+insert into conteudo(titulo, texto, data, usuario) values('Nossa ONG', 'Esta ong da melhor idade visa acompanhar programadores com experiencia', '2016-04-28', 1);
+insert into conteudo(titulo, texto, data, usuario) values('Nossa ONG 2', 'Esta ong da pior idade visa acompanhar programadores com experiencia', '2016-04-29', 1);
+insert into conteudo(titulo, texto, data, usuario) values('Nossa ONG 3', 'Esta ong da mais ou menos idade visa acompanhar programadores com experiencia', '2016-04-28', 1);
+insert into conteudo(titulo, texto, data, usuario) values('Nossa ONG 4', 'Esta ong da melhor idade visa acompanhar programadores com experiencia', '2016-04-27', 1);
+insert into conteudo(titulo, texto, data, usuario) values('Nossa ONG 5', 'Esta ong da melhor idade visa acompanhar programadores com experiencia', '2016-04-20', 1);
+insert into conteudo(titulo, texto, data, usuario) values('Nossa ONG 6', 'Esta ong da melhor idade visa acompanhar programadores com experiencia', '2016-04-15', 1);
+
 
