a007065fbe97763cd6e78e6fea722ff8e361e1db
shaq-senior-expert <senior.expert.iot@gmail.com>
2016-04-20 20:52:04 -0300

    O formulario de cadastro de idosos foi criado

---
+++ admin/cadastroidosos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br" ng-app="app">
+  <head>
+    <meta charset="utf-8">
+    <title>Portal Melhor Idade</title>
+
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/padrao.css">
+
+  </head>
+  <body>
+    <main>
+      <header class="menu">
+        <nav  ng-controller="contatoCtrl">
+          <ul>
+            <li ng-repeat="menu in contatos"><a href="#">{{menu.email}}</a></li>
+          </ul>
+        </nav>
+      </header>
+    </main>
+
+    <div>
+    	<form method="">
+        <label> Cadastro de idosos: </label>
+        <br />
+      		<input type="text" ng-model="nome" name="nome" placeholder="Digite seu nome completo" /> <br />
+          <input type="text" ng-model="endereco" name="endereco" placeholder="Digite seu Endereco" /><br />
+          <input type="email" ng-model="email" name="email" placeholder="Digite seu email" /><br />
+     
+      		<input type="submit" ng-model="enviar" value"OK"></input>
+  		</form>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js" ></script>
+    <script src="js/controller.js" ></script>
+  </body>
+</html>
