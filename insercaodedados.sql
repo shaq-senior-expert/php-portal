2dfa75ad8b4122885c623fb5a44a8b534d1ad31e
shaq-senior-expert <senior.expert.iot@gmail.com>
2016-04-20 19:22:39 -0300

    correcao da cnoecao

--- insercaodedados.sql
+++
@@ -1,8 +0,0 @@
-insert into contato(nome, email, assunto, mensagem) values('idoso1', 'idoso1@bol.com.br', 'reclamacao', 'Estou insatifeito com os servicos prestados');
-
-insert into contato(nome, email, assunto, mensagem) values('idoso2', 'idoso2@bol.com.br', 'puxacao de saco', 'Estou extremamente satifeito com os servicos prestados');
-
-insert into contato(nome, email, assunto, mensagem) values('idoso3', 'idoso3@bol.com.br', 'puxacao de saco', 'Estou extremamente satifeito com os servicos prestados');
-
-insert into contato(nome, email, assunto, mensagem) values('idoso4', 'idoso4@bol.com.br', 'puxacao de saco', 'Estou extremamente satifeito com os servicos prestados');
-
