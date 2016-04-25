91f5b8b077abd2e50af1c31cd9fbd742eb4b2b4b
shaq-senior-expert <senior.expert.iot@gmail.com>
2016-04-25 19:42:58 -0300

    Criacao do Layout utilizando bootstrap

--- admin/api_login.php
+++ admin/api_login.php
@@ -1,11 +1,28 @@
 <?php
-header('Content-Type: application/jason');
-require "../bd.php";
+
+$mysqli = mysqli_connect("127.0.0.1", "root", "123456", "PHPONG");
 
 function login(){
 $login = $_POST['login'];
 $senha = $_POST['senha'];
+
+echo $login;
+echo $senha;
 $query = "SELECT * FROM idoso where login={$login} and senha={$senha}";
+$linhas = array();
+			if($result = $mysqli->query($query)) { 
+				while ($row = $result->fetch_assoc()) {
+		    		$linhas[] = $row;  
+				}
+				
+			}
+			$i = 0;
+			do{
+
+				echo ($linhas[$i]);
+				$i++;
+			}while($i<=3);
 }
+
 
 ?>
--- admin/login.php
+++ admin/login.php
@@ -1,36 +1,52 @@
 <!DOCTYPE html>
-<html lang="pt-br" ng-app="app">
+<html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Portal Melhor Idade</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
 
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/padrao.css">
+    <title>Signin Template for Bootstrap</title>
+
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link href="bootstrap/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+
+    <link href="css/padrao.css" rel="stylesheet">
+
+    <script src="bootstrap/js/ie-emulation-modes-warning.js"></script>
+
 
   </head>
+
   <body>
-    <main>
-      <header class="menu">
-        <nav  ng-controller="contatoCtrl">
-          <ul>
-            <li ng-repeat="menu in contatos"><a href="#">{{menu.email}}</a></li>
-          </ul>
-        </nav>
-      </header>
-    </main>
 
-    <div>
-      
-    	<form action="api_login.php" method="post">
-        <label> Login: </label>
-        <br />
-      		<input type="text" ng-model="login" name="login" placeholder="Digite seu usuario" /> <br />
-      		<input type="password" ng-model="senha"name="senha" placeholder="Digite sua senha" /><br />
-      		<input type="submit" ng-model="enviar" value"OK"></input>
-  		</form>
-    </div>
+    <div class="container">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js" ></script>
-    <script src="js/controller.js" ></script>
+      <form class="form-signin">
+        <h2 class="form-signin-heading">Sr. Idoso por favor realize o Login</h2>
+        <label for="inputEmail" class="sr-only">Endereco de Email</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <label for="inputPassword" class="sr-only">Senha</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" value="remember-me"> Esqueceu a senha?
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="bootstrap/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
