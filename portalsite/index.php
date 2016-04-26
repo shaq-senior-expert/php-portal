e8f496865d9ec9ecb1e11d43db603778ed04e367
shaq-senior-expert <senior.expert.iot@gmail.com>
2016-04-25 21:06:16 -0300

    Ajuste

--- portalsite/index.php
+++ portalsite/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo "Hello, World";
+$pagina = "layout/index.php";
+
+if($_GET){
+	if(array_key_exists('pagina', $_GET)) {
+		$paginaGET = $_GET['pagina'];
+		$pagina = "htmls/pages/{$paginaGET}.php";
+	} else {
+		$pagina = "htmls/pages/404.php";		
+	}
+}
+
+$arquivo = "htmls/{$pagina}";
+if(file_exists($arquivo)) {
+	require_once $arquivo;
+} else {
+	require_once "htmls/pages/404.php";
+}
