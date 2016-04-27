1dfb8bd51e46d5b735b194c03ac9add613bd4b92
shaq-senior-expert <senior.expert.iot@gmail.com>
2016-04-26 21:23:42 -0300

    Ajuste controller

--- front/admin/js/controller/controller.js
+++ front/admin/js/controller/controller.js
@@ -1,6 +1,15 @@
 var app = angular.module('app', []);
 
-app.controller('contatoCtrl', function ($scope) {
-  $scope.contatos = [{"email":"home"},{"email":"Busca"},{"email":"Contato"}]
+app.controller('contatoCtrl', function ($scope, $http) {
+	$http.get('http://localhost/portalidadesa/portal/back/menuIdoso.php')
+    .success(function(retorno) {
+    	var titulo = retorno[0]['titulo'];
+    	var url = retorno[0]['url'];
+
+    	console.log(titulo, url);
+    })
+    .error(function(erro) {
+        console.log(erro);
+    });
 
 });
--- front/portalsite/htmls/includes/topo.php
+++ front/portalsite/htmls/includes/topo.php
@@ -1,1 +1,18 @@
-CABECALHO
+<!DOCTYPE html>
+<html lang="pt-br" ng-app="app">
+<head>
+	<meta charset="UTF-8">
+	<title>:: Melhor idade ::</title>
+</head>
+<body>
+	<header ng-controller="contatoCtrl">
+		<nav>
+			<ul ng-repeat="titulos in menu">
+				<li><a href="{{titulos.link}}">{{titulos.titulo}}</a></li>
+			</ul>
+		</nav>
+	</header>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js" ></script>
+    <script src="midia/js/controller/controller.js" ></script>
+</body>
+</html>
